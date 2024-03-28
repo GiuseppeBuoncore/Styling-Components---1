bff22d864d60850c0e235fc862698213c33089b0
GiuseppeBuoncore <135731752+GiuseppeBuoncore@users.noreply.github.com>
2024-03-28 18:26:53 +0100

    Add files via upload

---
+++ Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div className="welcome">
+      <h2>Welcome!</h2>
+      <p>Please, register or login.</p>
+    </div>
+  );
+};
+
+export default Welcome;
---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Welcome from './Welcome';
+import './index.css'; // Importiamo il file CSS qui
+
+ReactDOM.render(<Welcome />, document.getElementById('root'));
